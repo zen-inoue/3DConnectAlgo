14fb410a475857e2232428a1f97784ae2e1adbf1
丹澤 清 <tanzawa.kiyoshi@zen-integration.com>
2025-09-26 02:12:57 +0900

    test

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ class MyAI(Alg3D):
         player: int, # 先手(黒):1 後手(白):2
         last_move: Tuple[int, int, int] # 直前に置かれた場所(x, y, z)
     ) -> Tuple[int, int]:
-        return 3,3
         # 初期化
         self.do_initialize(board, player)
 
